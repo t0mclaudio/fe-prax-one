95336a8d4b89bb7cb9dae4e7f7f9c06488a3b1a3
Tom Claudio <dev.tomclaudio@gmail.com>
2021-12-24 11:48:15 +0800

    create fx to add new to do

--- src/context.js
+++ src/context.js
@@ -1,25 +1,39 @@
 import { useState, useEffect, createContext } from "react";
 
-export const AppContext = createContext({})
+export const AppContext = createContext({});
 
-const AppContextProvider = ({children}) => {
+const AppContextProvider = ({ children }) => {
   const [toDoList, setToDoList] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const addNewToDo = async (title) => {
+    const response = await fetch("http://localhost:3001/todo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+    const data = await response.json();
+    setToDoList([data, ...toDoList]);
+    setIsModalOpen(false);
+  };
 
   useEffect(() => {
     const fetchToDoList = async () => {
       const response = await fetch("http://localhost:3001/todo");
       const data = await response.json();
       setToDoList(data);
-    }
-    fetchToDoList()
-  }, [])
+    };
+    fetchToDoList();
+  }, []);
 
   return (
-    <AppContext.Provider value={{toDoList, isModalOpen, setIsModalOpen}}>
+    <AppContext.Provider
+      value={{ toDoList, isModalOpen, setIsModalOpen, addNewToDo }}
+    >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 
-export default AppContextProvider
+export default AppContextProvider;
